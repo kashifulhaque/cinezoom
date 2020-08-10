9a6a4aadc8075c1507c58df734cfb8f786f8ad4c
kashifulhaque <haque.kashiful7@gmail.com>
2020-08-10 16:33:05 +0530

    Initial commit

---
+++ src/main/java/xyz/ifkash/cinezoom/Cinezoom.java
@@ -0,0 +1,10 @@
+package xyz.ifkash.cinezoom;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Cinezoom implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("Cinezoom: initialized.");
+    }
+}
