ddc153fa3cfdb321aa54b2cbb14daac2c4fef9ec
kashifulhaque <haque.kashiful7@gmail.com>
2020-08-10 19:01:56 +0530

    Crash :/

--- src/main/java/xyz/ifkash/cinezoom/Cinezoom.java
+++ src/main/java/xyz/ifkash/cinezoom/Cinezoom.java
@@ -1,10 +1,70 @@
 package xyz.ifkash.cinezoom;
 
-import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.options.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
 
-public class Cinezoom implements ModInitializer {
+public class Cinezoom implements ClientModInitializer {
+
+    // Bool to keep track of Zoom and smooth camera
+    private static boolean isZoom = false;
+    private static boolean isSmooth = false;
+
+    // Key binding to trigger the zoom
+    private static KeyBinding key;
+    private static final int keyBind = GLFW.GLFW_KEY_Z;
+    private static final InputUtil.Type type = InputUtil.Type.KEYSYM;
+
+    // Get Minecraft instance
+    private static final MinecraftClient MINECRAFT = MinecraftClient.getInstance();
+
+    // Member variables
+    private static final String translationKey = "key.cinezoom.zoom";
+    private static final String category = "category.cinezoom.zoom";
+    private static final float zoomLevel = 20.0f;   // Optifine uses FOV 19 for it's zoom level
+
     @Override
-    public void onInitialize() {
-        System.out.println("Cinezoom: initialized.");
+    public void onInitializeClient() {
+        key = new KeyBinding(translationKey, type, keyBind, category);
+        KeyBindingHelper.registerKeyBinding(key);
     }
+
+    public static boolean isSmoothCamEnabled() { return MINECRAFT.options.smoothCameraEnabled; }
+
+    public static void enableSmoothCam() { MINECRAFT.options.smoothCameraEnabled = true; }
+
+    public static void disableSmoothCam() { MINECRAFT.options.smoothCameraEnabled = false; }
+
+    public static void resetSmoothCam() {
+        if (isSmooth) enableSmoothCam();
+        else disableSmoothCam();
+    }
+
+    public static void manageSmoothCam() {
+        if(isZoomStarting()) {
+            startZoom();
+            enableSmoothCam();
+        } else {
+            stopZoom();
+            disableSmoothCam();
+        }
+    }
+
+    public static boolean isKeyPressed() { return key.isPressed(); }
+
+    private static boolean isZoomStarting() { return isKeyPressed() && !isZoom; }
+
+    private static boolean isZoomStopping() { return !isKeyPressed() && isZoom; }
+
+    private static void startZoom() {
+        isSmooth = isSmoothCamEnabled();
+        isZoom = true;
+    }
+
+    private static void stopZoom() { isZoom = false; }
+
+    public static float getLevel() { return zoomLevel; }
 }
--- src/main/java/xyz/ifkash/cinezoom/mixin/CinezoomMixin.java
+++ src/main/java/xyz/ifkash/cinezoom/mixin/CinezoomMixin.java
@@ -1,15 +1,23 @@
 package xyz.ifkash.cinezoom.mixin;
 
-import net.minecraft.client.gui.screen.TitleScreen;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.render.GameRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import xyz.ifkash.cinezoom.Cinezoom;
 
-@Mixin(TitleScreen.class)
+@Mixin(GameRenderer.class)
+@Environment(EnvType.CLIENT)
 public class CinezoomMixin {
-    @Inject(at = @At("HEAD"), method = "init()V")
-    private void init(CallbackInfo info) {
-        System.out.println("CinezoomMixin: initialized.");
+    private void init(CallbackInfo info) { System.out.println("CinezoomMixin: initialized."); }
+
+    @Inject(method = "getFov(Lnet/minecraft/client/render/Camera;FZ)D", at = @At("HEAD"), cancellable = true)
+    public void getLevel(CallbackInfoReturnable<Float> info) {
+        if(Cinezoom.isKeyPressed()) info.setReturnValue(Cinezoom.getLevel());
+        Cinezoom.manageSmoothCam();
     }
 }
